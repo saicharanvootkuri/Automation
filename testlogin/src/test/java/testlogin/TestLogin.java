d3162cfb8bd13e74f33440cdab3c7af6d2466a24
saicharanvootkuri <vootkurisaicharan@gmail.com>
2024-02-15 17:15:26 +0530

    first commit

---
+++ testlogin/src/test/java/testlogin/TestLogin.java
@@ -0,0 +1,48 @@
+package testlogin;
+
+import java.time.Duration;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestLogin {
+
+	private static final Logger log = Logger.getLogger(TestLogin.class.getName());
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		login(driver);
+		driver.quit();
+	}
+
+	private static void login(WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login ");
+
+		driver.manage().window().maximize();
+
+		String username = "Admin";
+		String password = "admin123";
+
+		WebElement usernameField = wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//input[contains(@class, 'oxd-input')][@name='username']")));
+		WebElement passwordField = wait.until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//input[contains(@class, 'oxd-input')][@name='password']")));
+		WebElement loginButton = wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//button[contains(@class, 'orangehrm-login-button')]")));
+
+		usernameField.sendKeys(username);
+		passwordField.sendKeys(password);
+		loginButton.click();
+
+		log.info("Successfully logged in.");
+	}
+}
